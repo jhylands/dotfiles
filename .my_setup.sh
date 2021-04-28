bcb7294e6d6eed237d7f243c96a3c0a63be7182a
jhylands <hylands.james@gmail.com>
2021-04-28 09:52:20 +0100

    Added vim cypher syntax hilighting

--- .my_setup.sh
+++ .my_setup.sh
@@ -8,3 +8,7 @@ tmux send-keys "./~/.tmux/plugins/tpm/bin/install_plugins";
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd .vim/bundle
+git clone git://github.com/neo4j-contrib/cypher-vim-syntax.git ~/.vim/bundle/
