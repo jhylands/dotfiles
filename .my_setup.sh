1013ccfd64117bb239c3a4e6b2ca2c7ad28038b5
jhylands <hylands.james@gmail.com>
2021-05-10 14:49:13 +0000

    Forgot to include pathogen

--- .my_setup.sh
+++ .my_setup.sh
@@ -4,3 +4,5 @@ apt install r-base
 pip3 install ptpython yamllint flake8 mypy
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
